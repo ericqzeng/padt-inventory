6d72bb9e96c809153114ca8dc962ac5610ebce90
Eric Zeng <ericzeng501@gmail.com>
2019-11-18 19:03:03 -0500

    basic catelog query working

--- client/src/catalog.js
+++ client/src/catalog.js
@@ -1,23 +1,65 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import Input from '@material-ui/core/Input'
+import axios from 'axios'
+import { FormHelperText, Button, Select, TextField, FormControl, MenuItem } from '@material-ui/core';
 
+//TODO: maybe just the search component instead?
 class Catalog extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            name: '',
+            type: '',
+            // years: []
+        }
     }
 
     doSearch = (event) => {
-        console.log('execute serach!')
+        event.preventDefault()
+        console.log('execute serach...')
+        var query = {}
+        for (let e in this.state) {
+            if (this.state[e] !== null && this.state[e] !== '') {
+                query[e] = this.state[e]
+            }
+        }
+        console.log(query)
+        axios.get('/api/catalog', { params: query }).then(res => {
+            console.log(res)
+        }, err => {
+            alert(err)
+        });
+    }
+
+    changeHandler = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState({
+            [name]: value
+        });
     }
 
     render() {
         return (
             <div>
                 <form onSubmit={this.doSearch} >
-                    <Input placeholder='Name Search'></Input>
-                    <Button onClick={this.doSearch} color='primary'>ayyo</Button>
+                    <FormControl>
+                        <TextField name='name' value={this.state.name} onChange={this.changeHandler} placeholder='Item name' />
+                    </FormControl>
+                    <FormControl>
+                        <Select name='type' value={this.state.type} onChange={this.changeHandler}>
+                            <MenuItem value=''>Any</MenuItem>
+                            <MenuItem value='Costume'>Costume</MenuItem>
+                            <MenuItem value='Prop'>Prop</MenuItem>
+                            <MenuItem value='Gear'>Gear</MenuItem>
+                        </Select>
+                        <FormHelperText>Type</FormHelperText>
+                    </FormControl>
+                    {/* <FormControl>
+                        <TextField name="years" value={this.state.years} onChange={this.changeHandler}></TextField>
+                        TODO: maybe add year filters?
+                    </FormControl> */}
+                    <Button onClick={this.doSearch} color='primary'>Full send</Button>
                 </form>
 
             </div>
