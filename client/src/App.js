0fef6e6a5256c425209dd1070d48e1aa2e4a1465
Eric Zeng <ericzeng501@gmail.com>
2020-01-02 02:01:35 -0800

    small cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -13,22 +13,6 @@ import './css/styles.css'
 function App() {
   let store = createStore(reducer)
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     < Provider store={store} >
       <BrowserRouter>
         <Switch>
